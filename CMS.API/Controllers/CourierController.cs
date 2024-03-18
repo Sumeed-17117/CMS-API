78fd0ded2c8e188c9f3a3525ca989587f03ecd84
MuahmamdAliShaikh <119584309+MMohsin04@users.noreply.github.com>
2024-03-19 03:44:05 +0500

    Create,GetAll,GetById,Delete,Update

--- CMS.API/Controllers/CourierController.cs
+++ CMS.API/Controllers/CourierController.cs
@@ -1,6 +1,8 @@
 ﻿using CMS.DBServices.Interfaces;
+using CMS.DBServices.Services;
 using CMS.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
 
 namespace CMS.API.Controllers
 {
@@ -19,11 +21,21 @@ namespace CMS.API.Controllers
         {
             try
             {
-                var savedCourier = await _courierService.CreateCourier(courier);
-                return Ok(savedCourier);
-            }catch(Exception ex)
+                var existingCourier = await _courierService.SearchCourierByCourierName(courier.CourierName);
+
+                if (existingCourier != null)
+                {
+                    return Conflict(new { Message = "Courier Already Exist" });
+                }
+                else
+                {
+                    var savedCourier = await _courierService.CreateCourier(courier);
+                    return Ok(savedCourier);
+                }
+            }
+            catch (Exception ex)
             {
-                return BadRequest(ex.Message);  
+                return BadRequest(ex.Message);
             }
         }
 
@@ -35,6 +47,66 @@ namespace CMS.API.Controllers
             {
                 var courierList = await _courierService.GetAll();
                 return Ok(courierList);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetCourier/{CourierId:int}")]
+        public async Task<IActionResult> GetById(int CourierId)
+        {
+            try
+            {
+                var FoundedCourier = await _courierService.GetCourierById(CourierId);
+                if (FoundedCourier != null)
+                {
+                    return Ok(FoundedCourier);
+                }
+                return NotFound(new { Message = "Courier Not Found" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [HttpDelete]
+        [Route("DeleteCourier/{CourierId:int}")]
+        public async Task<IActionResult> DeleteCourier(int CourierId)
+        {
+            try
+            {
+                var FoundedCourier = await _courierService.GetCourierById(CourierId);
+                if (FoundedCourier == null)
+                {
+                    return BadRequest(new { Message = "Courier Not Found" });
+                }
+                await _courierService.Delete(FoundedCourier);
+                return Ok(new { Message = "Courier Deleted" });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        [HttpPut]
+        [Route("UpdateCourier/{CourierId:int}")]
+        public async Task<IActionResult> UpdateCourier([FromBody] Courier courierUpdate, int CourierId)
+        {
+            try
+            {
+                var FoundedCourier = await _courierService.GetCourierById(CourierId);
+                if (FoundedCourier == null)
+                {
+                    return BadRequest(new { Message = "Courier Not Found" });
+                }
+                await _courierService.Update(FoundedCourier, courierUpdate);
+                return Ok(new { Message = "Courier Updated" });
             }
             catch (Exception ex)
             {
